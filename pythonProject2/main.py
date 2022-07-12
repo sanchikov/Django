4d18b5f9c05d22a289c9d5a6d71ef9103d012cd4
sanchikov <anchikov@gmail.com>
2022-07-13 00:50:20 +0300

    Первая поппытка

--- pythonProject2/main.py
+++
@@ -1,8 +0,0 @@
-class Author(models.Model):
-    full_name = models.CharField()
-    name = models.CharField(null=True)
-
-    def some_method(self):
-        self.name = self.full_name.split()[0]
-
-    self.save()
